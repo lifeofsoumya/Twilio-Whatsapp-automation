ea1495ad3901edac1177225c49d4fb742b850b17
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-08-08 17:07:39 +0530

    dynamic number added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
@@ -6,6 +7,12 @@ const app = express();
 app.get('/', (req, res)=>{
     res.send('working');
 })
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 
 const PORT = process.env.port || 3000;
 
@@ -16,16 +23,18 @@ app.listen(PORT,()=>{
 const accountSid = process.env.TWILIO_ACCOUNT_SID; 
 const authToken = process.env.TWILIO_AUTH_TOKEN; 
 
-const client = require('twilio')(accountSid, authToken); 
+const client = require('twilio')(accountSid, authToken, { 
+    lazyLoading: true 
+});
 
 
-function sendMsg(){
+function sendMsg(message, senderID){
 
     client.messages 
     .create({ 
         body: "Soumya, You've a task", 
         from: 'whatsapp:+14155238886',       
-        to: 'whatsapp:+919734174147' 
+        to: senderID
     }) 
     .then(message => console.log(message.sid)) 
     .done();
@@ -33,3 +42,23 @@ function sendMsg(){
 }
 
 // sendMsg()
+
+
+// Route for WhatsApp
+app.post('/whatsapp', async (req, res) => {
+
+    let message = req.body.Body;
+    let senderID = req.body.From;
+
+    console.log(message);
+    console.log(senderID);
+
+    // Write a function to send message back to WhatsApp
+    if(message=='hi' || 'hello' || 'hi there'){
+        await sendMsg('Hello Soumya', senderID);
+    }
+    else if(message=='time'){
+        await sendMsg('The current time is ', senderID);
+    }
+
+});
