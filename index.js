fc6b056d76e555a375f8a9a219b2b0b9cf234bc4
Techn0mate <96782835+techn0mate@users.noreply.github.com>
2022-08-08 20:04:50 +0530

    message multiples removed

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post('/whatsapp', async (req, res) => { // creates webhook
         await sendMsg('Hello Soumya', senderID);
         message = "";
     }
-    else if(message==='link' || 'links'){
+    else if(message==='link'){
         await sendMsg('Your links are here: https://soumyamondal.com/link', senderID);
         message = "";
     }
@@ -94,7 +94,7 @@ app.post('/whatsapp', async (req, res) => { // creates webhook
         await sendMsg('Starting your day with curated news for you', senderID);
         message = "";
     }
-    else if(message==='projects' || 'chat app'){
+    else if(message==='projects'){
         await sendMsg('Here is your chat app: ⚪ https://soumyamondal.com/project/chatapp; Stock Notifier: ⚪ https://soumyamondal.com/project/stockalert', senderID);
         message = "";
     }
@@ -110,11 +110,11 @@ app.post('/whatsapp', async (req, res) => { // creates webhook
         await sendMsg('Here are your websites: ⚪ https://soumyamondal.com; ⚪ https://indgeek.com; ⚪ https://turbohosty.com; ⚪ https://pepeso.com; ⚪ https://ecoesports.com; ⚪ https://truelancing.com; ⚪ https://metatool.in; ⚪ https://nated.in; ', senderID);
         message = "";
     }
-    else if(message==='expenses' || 'expense' || 'cost'){
+    else if(message==='expense'){
         await sendMsg(`Your upcoming expenses: ${expenses}`, senderID);
         message = "";
     }
-    else if(message==='okay' || 'thanks' || 'ok'){
+    else if(message==='okay'){
         await sendMsg('Chalo bye, cya', senderID);
         message = "";
     }
