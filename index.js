3c8baa7a233e2fe0833028ed75ec12659be6f85d
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-08-08 19:08:53 +0530

    almost updated

--- index.js
+++ index.js
@@ -42,10 +42,13 @@ function sendMsg(message, senderID){
 }
 
 // sendMsg()
-
+const fetchExpenses = ()=>{
+    let expenses = "Current situation: Electric Bill 3300, Powerbank 1600, Invest 3000, Hdd 4500, Hosting 2000, Raspi 9500, Cargo 1200, Travel 10000, renewal domain 6000"
+}
+fetchExpenses()
 
 // Route for WhatsApp
-app.post('/whatsapp', async (req, res) => {
+app.post('/whatsapp', async (req, res) => { // creates webhook
 
     let message = req.body.Body;
     let senderID = req.body.From;
@@ -54,11 +57,50 @@ app.post('/whatsapp', async (req, res) => {
     console.log(senderID);
 
     // Write a function to send message back to WhatsApp
-    if(message=='hi' || 'hello' || 'hi there'){
+    if(message=='command' || 'commands' || 'list'){
+        await sendMsg('Use the Following commands: Hi, link, resume, github, turn off light, list:, post, my day, projects, indgeek, my site, sites', senderID);
+    }
+    else if(message=='hi' || 'hello' || 'hi there'){
         await sendMsg('Hello Soumya', senderID);
     }
-    else if(message=='time'){
-        await sendMsg('The current time is ', senderID);
+    else if(message=='link' || 'links'){
+        await sendMsg('Your links are here: https://soumyamondal.com/link', senderID);
+    }
+    else if(message=='resume'){
+        await sendMsg('Your Resume link: https://soumyamondal.com/resume', senderID);
+    }
+    else if(message=='github'){
+        await sendMsg('Your github: https://github.com/lifeofsoumya', senderID);
+    }
+    else if(message=='turn off light'){
+        await sendMsg('Your light is being turned off', senderID);
+    }
+    else if(message=='list:'){
+        await sendMsg('Here is your list:', senderID);
+    }
+    else if(message=='post'){
+        await sendMsg('Your photo was posted to all social media Platforms', senderID);
+    }
+    else if(message=='my day'){
+        await sendMsg('Starting your day with curated news for you', senderID);
+    }
+    else if(message=='projects' || 'chat app'){
+        await sendMsg('Here is your chat app: ⚪ https://soumyamondal.com/project/chatapp; Stock Notifier: ⚪ https://soumyamondal.com/project/stockalert', senderID);
+    }
+    else if(message=='my site'){
+        await sendMsg('Here are your site useful links: ⚪ https://soumyamondal.com/about; ⚪ https://soumyamondal.com/newsletter; ⚪ https://soumyamondal.com/blog; ⚪ https://soumyamondal.com/admin; ⚪ https://soumyamondal.com/coffee; ⚪ https://soumyamondal.com/courses; ⚪ https://soumyamondal.com/faq; ', senderID);
+    }
+    else if(message=='indgeek'){
+        await sendMsg('Here are IndGeek useful links: ⚪ https://indgeek.com/index; ⚪ https://indgeek.com/about-us/; ⚪ https://indgeek.com/contact; ⚪ https://indgeek.com/sitemap/; ⚪ https://indgeek.com/intern/; ⚪ https://indgeek.com/forum/; ', senderID);
+    }
+    else if(message=='sites'){
+        await sendMsg('Here are your websites: ⚪ https://soumyamondal.com; ⚪ https://indgeek.com; ⚪ https://turbohosty.com; ⚪ https://pepeso.com; ⚪ https://ecoesports.com; ⚪ https://truelancing.com; ⚪ https://metatool.in; ⚪ https://nated.in; ', senderID);
+    }
+    else if(message=='expenses' || 'expense' || 'cost'){
+        await sendMsg(`Your upcoming expenses: ${expenses}`, senderID);
+    }
+    else if(message=='okay' || 'thanks' || 'ok'){
+        await sendMsg('Chalo bye, cya', senderID);
     }
 
 });
