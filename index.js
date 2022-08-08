1dbbd1c2e714b6856674989f040e767c7c3e98ed
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-08-08 16:29:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+require('dotenv').config();
+
+const app = express();
+
+app.get((req, res)=>{
+    res.send('working');
+})
+
+const PORT = process.env.port || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`listening to Port: ${PORT}`)
+})
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID; 
+const authToken = process.env.TWILIO_AUTH_TOKEN; 
+
+const client = require('twilio')(accountSid, authToken); 
+
+
+function sendMsg(){
+
+    client.messages 
+    .create({ 
+        body: "Soumya, You've a task", 
+        from: 'whatsapp:+14155238886',       
+        to: 'whatsapp:+919734174147' 
+    }) 
+    .then(message => console.log(message.sid)) 
+    .done();
+
+}
+
+// sendMsg()
